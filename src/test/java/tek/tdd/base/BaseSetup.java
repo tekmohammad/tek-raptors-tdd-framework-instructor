c7a551b1341e5dda55cb17e8ee714e29334b0963
Mohammad Shokriyan <mohammad@tekschool.us>
2023-12-20 22:46:12 -0500

    add cross browsing

--- src/test/java/tek/tdd/base/BaseSetup.java
+++ src/test/java/tek/tdd/base/BaseSetup.java
@@ -2,18 +2,64 @@ package tek.tdd.base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Properties;
 
 public class BaseSetup {
 
     private static WebDriver driver;
 
     public void openBrowser() {
-        driver = new ChromeDriver();
+        Properties configs = readProperties();
+        String browserType = configs.getProperty("browser");
+        Boolean headless = Boolean.valueOf(configs.getProperty("headless"));
+        if (browserType.equalsIgnoreCase("chrome")) {
+            ChromeOptions options = new ChromeOptions();
+            if (headless) {
+                options.addArguments("--headless");
+            }
+            driver = new ChromeDriver(options);
+        } else if (browserType.equalsIgnoreCase("edge")) {
+            EdgeOptions options = new EdgeOptions();
+            if (headless) {
+                options.addArguments("--headless");
+            }
+            driver = new EdgeDriver(options);
+        } else if (browserType.equalsIgnoreCase("firefox")) {
+            FirefoxOptions options = new FirefoxOptions();
+            if (headless) {
+                options.addArguments("--headless");
+            }
+            driver = new FirefoxDriver(options);
+        } else {
+            throw new RuntimeException("Wrong browser type check config file");
+        }
+
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.get("https://retail.tekschool-students.com/");
+        String url = configs.getProperty("base.url");
+        driver.get(url);
+    }
+
+    private Properties readProperties() {
+        String propertyFilePath = System.getProperty("user.dir")
+                + "/src/test/resources/config/qa-env.properties";
+        Properties properties = new Properties();
+        try {
+            FileInputStream inputStream = new FileInputStream(propertyFilePath);
+            properties.load(inputStream);
+        }catch (IOException ex) {
+            throw new RuntimeException("Not able to read property file " + ex.getMessage());
+        }
+        return properties;
     }
 
     public void closeBrowser() {
--- src/test/java/tek/tdd/base/UITestsBase.java
+++ src/test/java/tek/tdd/base/UITestsBase.java
@@ -1,6 +1,11 @@
 package tek.tdd.base;
 
+import com.aventstack.extentreports.MediaEntityBuilder;
+import com.aventstack.extentreports.service.ExtentTestManager;
 import com.aventstack.extentreports.testng.listener.ExtentITestListenerClassAdapter;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
@@ -21,7 +26,16 @@ public class UITestsBase extends SeleniumUtilities {
     }
 
     @AfterMethod
-    public void testClosure() {
+    public void testClosure(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            TakesScreenshot takesScreenshot = (TakesScreenshot) getDriver();
+            String screenshot = takesScreenshot.getScreenshotAs(OutputType.BASE64);
+            ExtentTestManager.getTest()
+                    .fail("Test Failed Taking Screen shot" ,
+                            MediaEntityBuilder.createScreenCaptureFromBase64String(
+                                    screenshot,
+                                    "failed_test_screen_shot").build());
+        }
         closeBrowser();
     }
 }
